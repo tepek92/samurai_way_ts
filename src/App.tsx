93799c9fe7b3a8987094ddd835dec02f44c1146c
tepek92 <tepek92@mail.ru>
2022-09-12 02:50:19 +0700

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {Technologies} from "./Technologies";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <Header/>
+            <Technologies/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div>
+            <a href="#">Home </a>
+            <a href="#">New Feed </a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
