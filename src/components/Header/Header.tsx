d45eb63d99ee93f35f9ed3b1884fc225553f15b7
tepek92 <tepek92@mail.ru>
2023-02-07 22:44:28 +0700

    refactor(Header): buttons link style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import React, {FC} from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation, useRouteMatch} from "react-router-dom";
 import {HeaderPropsType} from "./HeaderContainer";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
 import style from './Header.module.css';
 import avatar from "../../img/samurai2.png";
 import Chip from '@mui/material/Chip';
@@ -18,35 +16,67 @@ export const Header: FC<HeaderPropsType> = ({
                                               isAuth,
                                               deleteLogin,
                                               profile,
-}) => {
+                                            }) => {
+  const location = useLocation();
   const logoutHandler = () => {
     deleteLogin();
   }
+
+  const profileStyle = location.pathname === '/profile' ? 'contained' : 'outlined'
+  const dialogsStyle = location.pathname === '/dialogs' ? 'contained' : 'outlined'
+  const usersStyle = location.pathname === '/users' ? 'contained' : 'outlined'
+
   return (
     <Box>
       <AppBar color="inherit" position="static">
         <Toolbar>
           <nav className={style.nav}>
-            <div className={style.link}><NavLink to={"/profile"} activeClassName={style.active}>Profile</NavLink></div>
-            <div className={style.link}><NavLink to={"/dialogs"} activeClassName={style.active}>Messages</NavLink></div>
-            <div className={style.link}><NavLink to={"/users"} activeClassName={style.active}>Users</NavLink></div>
+            <NavLink
+              to={"/profile"}
+              activeClassName={style.active}>
+              <Button
+                variant={profileStyle}
+                sx={{ml: 2}}
+              >
+                Profile
+              </Button>
+            </NavLink>
+
+            <NavLink
+              to={"/dialogs"}
+              activeClassName={style.active}
+            >
+              <Button
+                variant={dialogsStyle}
+                sx={{ml: 2}}
+              >
+                Messages
+              </Button>
+            </NavLink>
+
+            <NavLink
+              to={"/users"}
+              activeClassName={style.active}
+            >
+              <Button
+                variant={usersStyle}
+                sx={{ml: 2}}
+              >
+                Users
+              </Button>
+            </NavLink>
           </nav>
           {isAuth &&
               <Box sx={{marginLeft: 'auto'}}>
-                  {/*<Tooltip title={login}>*/}
-                  {/*    <IconButton sx={{mr: 1}}>*/}
-                  {/*        <Avatar alt="Remy Sharp" src={profile.photos?.large ? profile.photos.large : avatar}/>*/}
-                  {/*    </IconButton>*/}
-                  {/*</Tooltip>*/}
-
                   <Chip
                       avatar={<Avatar alt="avatar" src={profile.photos?.large ? profile.photos.large : avatar}/>}
                       label={profile.fullName}
                       variant="outlined"
                   />
                   <Button
-                      color="inherit"
+                      variant='outlined'
                       onClick={logoutHandler}
+                      sx={{ml: 2}}
                   >
                       Logout
                   </Button>
